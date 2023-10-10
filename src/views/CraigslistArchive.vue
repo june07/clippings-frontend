ed24ee55d438417082d4bfc90ce336a6c4b6eb0d
Will <will@onezerohosting.com>
2023-10-10 12:21:50 -0700

    .

--- src/views/CraigslistArchive.vue
+++ src/views/CraigslistArchive.vue
@@ -1,32 +1,60 @@
 <template>
     <v-container class="h-100 d-flex align-center justify-center flex-column" fluid>
-        <p :class="smAndDown ? 'text-h6' : 'text-h5'" class="mb-4">Clippings is an archival tool for online classified ads like Craigslist!</p>
-        <p :class="smAndDown ? 'text-body-2' : 'text-body-1'">It takes a snapshot of the ad and saves it to the cloud so that it will always be available even if the original poster (or anyone else) removes it.</p>
-        <p :class="smAndDown ? 'text-body-2' : 'text-body-1'">
-        </p>
-
-        <v-card rounded="xl" class="pa-4" :width="smAndDown ? '-webkit-fill-available' : '800px'">
-            <v-card-title class="font-weight-light text-center">Archive an Ad</v-card-title>
-            <v-card-subtitle class="font-weight-light text-center">Enter the link to the ad you want to archive</v-card-subtitle>
-            <v-card-text>
-                <v-text-field validate-on="lazy" density="compact" variant="solo" rounded="lg" v-model="store.textField" persistent-hint hint="Any Craigslist ad link" placeholder="https://sfbay.craigslist.org/sfc/zip/d/ad-to-archive" :rules="rules.url" />
+        <v-card rounded="xl" class="pa-4" :width="smAndDown ? '-webkit-fill-available' : '800px'" elevation="0" v-if="location.pathname === '/home'">
+            <p :class="smAndDown ? 'text-h5' : 'text-h4'" class="mb-8"><span class="font-weight-bold">Clippings</span><span class="font-italic"> is your modern-day archival tool for online classified ads...</span> like Craigslist!</p>
+            <p :class="smAndDown ? 'text-body-2' : 'text-body-1'">Clippings is your modern-day archival tool for online classified ads, reminiscent of the days when we used to clip newspaper ads. It captures and securely stores ad snapshots in the cloud, ensuring they're always accessible—even if the original poster or anyone else removes them.</p>
+            <div class="d-flex justify-center">
+                <ul class="mt-4 font-weight-bold text-body-2">
+                    <li>Ads archived to robust/permanent cloud location (<a href="https://github.com/" rel="noopener" target="_blank">GitHub</a>)</li>
+                    <li>No fussing with screenshots or storing files on your own device</li>
+                    <li>100% open source (Vue.js <a href="https://github.com/june07/clippings-frontend" rel="noopener" target="_blank">frontend</a>,
+                        Node.js <a href="https://github.com/june07/clippings-backend" rel="noopener" target="_blank">backend</a>,
+                        <a href="https://github.com/june07/clippings-archive" rel="noopener" target="_blank">archive</a>)
+                    </li>
+                </ul>
+            </div>
+            <p :class="smAndDown ? 'text-body-2' : 'text-body-1'" class="mb-8">
+            </p>
+        </v-card>
+        <v-spacer />
+        <v-card rounded="xl" class="pa-4" :width="smAndDown ? '-webkit-fill-available' : '800px'" elevation="0" v-if="mostRecentListingPids.length">
+            <v-card-title class="font-weight-light text-center">Most recently archived Ads</v-card-title>
+            <v-card-text v-if="!loading.archive" class="font-weight-light text-center">
+                <div v-for="mostRecentListingPid of mostRecentListingPids">
+                    <v-btn v-if="!smAndDown" density="compact" variant="text" prepend-icon="link" :href="`https://clippings-archive.june07.com/craigslist/${mostRecentListingPid}`" target="_blank">
+                        <div class="text-caption text-truncate">{{ `https://clippings-archive.june07.com/craigslist/${mostRecentListingPid}` }}</div>
+                    </v-btn>
+                    <a v-else style="text-decoration: none" :href="`https://clippings-archive.june07.com/craigslist/${mostRecentListingPid}`" target="_blank">
+                        <div class="text-caption text-truncate">
+                            <v-icon icon="link" class="mr-2" />{{ `https://clippings-archive.june07.com/craigslist/${mostRecentListingPid}` }}
+                        </div>
+                    </a>
+                </div>
+            </v-card-text>
+        </v-card>
+        <v-card rounded="xl" class="pa-4" :width="smAndDown ? '-webkit-fill-available' : '800px'" :elevation="loading.archive ? 0 : undefined">
+            <v-card-title class="font-weight-light text-center">{{ loading.archive ? 'Archiving' : 'Archive an' }} Ad</v-card-title>
+            <v-card-subtitle v-if="!loading.archive" class="font-weight-light text-center">Enter the link to the ad you want to archive</v-card-subtitle>
+            <v-card-subtitle v-else class="font-weight-light text-center font-caption text-wrap">{{ smAndDown ? shortenAdURL(store.textField) : store.textField }}</v-card-subtitle>
+            <v-card-text v-show="!loading.archive" class="pa-0">
+                <v-text-field validate-on="lazy" density="compact" variant="solo" roundesd="lg" v-model="store.textField" persistent-hint hint="Any Craigslist ad link" placeholder="https://sfbay.craigslist.org/sfc/zip/d/ad-to-archive" :rules="rules.url" />
                 <div v-if="archiveData[listingPid] || archiveWaitingToBeReady !== undefined" class="text-center">
-                    <div class="text-h6 my-4">Ad saved.</div>
-                    <p class="text-start mb-4">By saving this ad you've ensured that it'll be around to reference later. The data is saved to the cloud and accessible via the links below:</p>
+                    <div class="text-h6 my-4">📰 Ad saved. 🗞️</div>
+                    <p class="text-start mb-4">The data is saved to the cloud and will be accessible via the links below once they turn green:</p>
                     <v-row class="d-flex align-center">
                         <v-spacer />
                         <v-col cols="2" class="text-end text-caption pb-0">web</v-col>
                         <v-col :cols="smAndDown ? 10 : 9" class="text-start pb-0 pl-0">
-                            <v-btn v-if="!smAndDown" density="compact" variant="text" prepend-icon="link" :color="archiveWaitingToBeReady ? 'grey-lighten-2' : 'green'" :href="`https://jc-archive.june07.com/craigslist/${listingPid}`" target="_blank" :loading="archiveWaitingToBeReady">
-                                <div class="text-caption text-truncate">{{ `https://jc-archive.june07.com/craigslist/${listingPid}` }}</div>
+                            <v-btn v-if="!smAndDown" density="compact" variant="text" prepend-icon="link" :color="archiveWaitingToBeReady ? 'grey-lighten-2' : 'green'" :href="`https://clippings-archive.june07.com/craigslist/${listingPid}`" target="_blank" :loading="archiveWaitingToBeReady">
+                                <div class="text-caption text-truncate">{{ `https://clippings-archive.june07.com/craigslist/${listingPid}` }}</div>
                                 <template v-slot:loader>
-                                    <v-progress-circular size="x-small" width="1" class="mr-2" indeterminate />
-                                    <div class="text-caption text-truncate">{{ `https://jc-archive.june07.com/craigslist/${listingPid}` }}</div>
+                                    <v-progress-circular color="amber" size="x-small" width="1" class="mr-2" indeterminate />
+                                    <div class="text-caption text-truncate">{{ `https://clippings-archive.june07.com/craigslist/${listingPid}` }}</div>
                                 </template>
                             </v-btn>
-                            <a v-else style="text-decoration: none" :color="archiveWaitingToBeReady ? 'grey-lighten-2' : 'green'" :href="`https://jc-archive.june07.com/craigslist/${listingPid}`" target="_blank">
+                            <a v-else style="text-decoration: none" :color="archiveWaitingToBeReady ? 'grey-lighten-2' : 'green'" :href="`https://clippings-archive.june07.com/craigslist/${listingPid}`" target="_blank">
                                 <div class="text-caption text-truncate" :class="archiveWaitingToBeReady ? 'text-grey-lighten-2' : 'text-green'">
-                                    <v-icon icon="link" class="mr-2" />{{ `https://jc-archive.june07.com/craigslist/${listingPid}` }}
+                                    <v-icon icon="link" class="mr-2" />{{ `https://clippings-archive.june07.com/craigslist/${listingPid}` }}
                                 </div>
                             </a>
                         </v-col>
@@ -36,16 +64,16 @@
                         <v-spacer />
                         <v-col cols="2" class="text-end text-caption py-0">git</v-col>
                         <v-col :cols="smAndDown ? 10 : 9" class="text-start py-0 pl-0">
-                            <v-btn v-if="!smAndDown" density="compact" variant="text" prepend-icon="link" :color="archiveWaitingToBeReady ? 'grey-lighten-2' : 'green'" :href="`https://github.com/june07/jc-archive/tree/main/craigslist/${listingPid}`" target="_blank" :loading="archiveWaitingToBeReady">
-                                <div class="text-caption text-truncate">{{ `https://github.com/june07/jc-archive/tree/main/craigslist/${listingPid}` }}</div>
+                            <v-btn v-if="!smAndDown" density="compact" variant="text" prepend-icon="link" :color="archiveWaitingToBeReady ? 'grey-lighten-2' : 'green'" :href="`https://github.com/june07/clippings-archive/tree/main/craigslist/${listingPid}`" target="_blank" :loading="archiveWaitingToBeReady">
+                                <div class="text-caption text-truncate">{{ `https://github.com/june07/clippings-archive/tree/main/craigslist/${listingPid}` }}</div>
                                 <template v-slot:loader>
-                                    <v-progress-circular size="x-small" width="1" class="mr-2" indeterminate />
-                                    <div class="text-caption text-truncate">{{ `https://github.com/june07/jc-archive/tree/main/craigslist/${listingPid}` }}</div>
+                                    <v-progress-circular color="amber" size="x-small" width="1" class="mr-2" indeterminate />
+                                    <div class="text-caption text-truncate">{{ `https://github.com/june07/clippings-archive/tree/main/craigslist/${listingPid}` }}</div>
                                 </template>
                             </v-btn>
-                            <a v-else style="text-decoration: none" :href="`https://github.com/june07/jc-archive/tree/main/craigslist/${listingPid}`" target="_blank">
+                            <a v-else style="text-decoration: none" :href="`https://github.com/june07/clippings-archive/tree/main/craigslist/${listingPid}`" target="_blank">
                                 <div class="text-caption text-truncate" :class="archiveWaitingToBeReady ? 'text-grey-lighten-2' : 'text-green'">
-                                    <v-icon icon="link" class="mr-2" />{{ `https://github.com/june07/jc-archive/tree/main/craigslist/${listingPid}` }}
+                                    <v-icon icon="link" class="mr-2" />{{ `https://github.com/june07/clippings-archive/tree/main/craigslist/${listingPid}` }}
                                 </div>
                             </a>
                         </v-col>
@@ -54,10 +82,21 @@
                 </div>
             </v-card-text>
             <v-card-actions class="justify-center mt-8">
-                <v-btn v-if="!archiveData[listingPid]" variant="tonal" @click="archiveHandler" :loading="loading.archive" :disabled="archiveData[listingPid] || archiveWaitingToBeReady !== undefined ? true : false">save forever</v-btn>
-                <v-btn v-else variant="tonal" @click="resetHandler">save another</v-btn>
+                <v-progress-circular v-if="loading.archive" indeterminate :size="200" width="1" color="amber">
+                    <v-btn v-if="!archiveData[listingPid]" :variant="loading.archive ? 'plain' : 'tonal'" @click="archiveHandler" :disabled="archiveData[listingPid] || archiveWaitingToBeReady !== undefined ? true : false">
+                        <div class="text-weight-light" :class="loading.archive ? 'animate__animated animate__pulse animate__infinite' : ''">{{ loading.archive ? 'saving' : 'save' }}</div>
+                    </v-btn>
+                    <v-btn v-else variant="tonal" @click="resetHandler">save another</v-btn>
+                </v-progress-circular>
+                <div v-else>
+                    <v-btn v-if="!archiveData[listingPid]" variant="tonal" rounded @click="archiveHandler" :disabled="archiveData[listingPid] || archiveWaitingToBeReady !== undefined ? true : false">
+                        <div class="text-weight-light" :class="loading.archive ? 'animate__animated animate__pulse animate__infinite' : ''">{{ loading.archive ? 'saving' : 'save' }}</div>
+                    </v-btn>
+                    <v-btn v-else variant="tonal" @click="resetHandler">save another</v-btn>
+                </div>
             </v-card-actions>
         </v-card>
+        <v-spacer />
     </v-container>
 </template>
 <style scoped></style>
@@ -67,6 +106,7 @@ import { useAppStore } from '@/store/app'
 import { useDisplay } from 'vuetify/lib/framework.mjs'
 import io from 'socket.io-client'
 import cookie from 'cookie'
+import 'animate.css'
 
 const { $api } = getCurrentInstance().appContext.config.globalProperties
 const intervals = ref({
@@ -88,6 +128,8 @@ const { VITE_API_SERVER } = import.meta.env
 const loading = ref({
     archive: false,
 })
+const mostRecentListingPids = ref([])
+const location = ref({})
 const debug = ref({})
 const isMounted = ref(false)
 const store = useAppStore()
@@ -123,6 +165,7 @@ const sio = io(VITE_API_SERVER + '/', {
         console.log(reason)
     })
 const pidFromUrl = (url) => url && url.match(/\/([^\/]*)\.html/)?.[1]
+const shortenAdURL = (url) => url && url.match(/https?:\/\/[^/]*(.*)/)?.[1] ? `...${url.match(/https?:\/\/[^/]*(.*)/)[1]}` : ''
 function archiveHandler() {
     if (!listingPid.value || !/https:\/\/.*\.craigslist\.org\/.+/.test(store.textField)) return
     loading.value.archive = true
@@ -144,6 +187,7 @@ watch(() => store.textField, (newValue, oldValue) => {
     })
 })
 onMounted(() => {
+    location.value = document.location
     if (/\/share/.test(document.location.pathname)) {
         const url = new URLSearchParams(document.location.search).get('url')
         const title = new URLSearchParams(document.location.search).get('title')
@@ -170,6 +214,9 @@ onMounted(() => {
                 archiveData.value[listingPid.value] = JSON.parse(archive)
             })
         }
+        sio.emit('getMostRecentListingPids', pids => {
+            mostRecentListingPids.value = pids
+        })
     })
         .on('update', (payload) => {
             const { archived } = payload
